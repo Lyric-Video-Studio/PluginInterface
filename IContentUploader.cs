7101432d545ee8ef452dc5974712f9decc98e329
Matti Pulkkinen <pulkkinen@ultimatium.com>
2024-11-05 23:08:32 +0200

    Support for cnd (WIP)

---
+++ IContentUploader.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace PluginBase
+{
+    /// <summary>
+    /// You can request app to upload content to user chosen CDN. You can call this interface withotu checking if the file is already uploaded,
+    /// the interface will return immediately if so. If file is local file, it is uploaded. Note that uploading might fails, so catch the http status code
+    /// User might have not activated any content delivery methods, in that case interface will return true is 'is local file' and you must handle error message for the user
+    /// Preferred error message is: 'Image needs to be publicly availabe link or you must enable content delivert from Settings'
+    /// </summary>
+    public interface IContentUploader
+    {
+        Task<(HttpStatusCode responseCode, string uploadedUrl, bool isLocalFile)> RequestContentUpload(string url);
+    }
+}
