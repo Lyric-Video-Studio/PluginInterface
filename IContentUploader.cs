6c80aefa3af65747e9f81168f68731095a9814ce
Matti Pulkkinen <pulkkinen@ultimatium.com>
2024-11-06 22:51:39 +0200

    Small clarification

--- IContentUploader.cs
+++ IContentUploader.cs
@@ -3,13 +3,14 @@
 namespace PluginBase
 {
     /// <summary>
-    /// You can request app to upload content to user chosen CDN. You can call this interface withotu checking if the file is already uploaded,
-    /// the interface will return immediately if so. If file is local file, it is uploaded. Note that uploading might fails, so catch the http status code
+    /// You can request app to upload content to user chosen CDN. You can call this interface without checking if the file is already uploaded,
+    /// the interface will return immediately if so. File should always be the local shource file, let interface implementation do it's stuff. Note that uploading might fail, so catch the http status code
     /// User might have not activated any content delivery methods, in that case interface will return true is 'is local file' and you must handle error message for the user
-    /// Preferred error message is: 'Image needs to be publicly availabe link or you must enable content delivert from Settings'
+    /// Preferred error message is: 'Image needs to be publicly availabe link or you must enable content delivery from Settings'
+    /// It is recommended that in your payload, only show the local source file for user and not store the url given from interface, since it might be temporary link
     /// </summary>
     public interface IContentUploader
     {
-        Task<(HttpStatusCode responseCode, string uploadedUrl, bool isLocalFile)> RequestContentUpload(string url);
+        Task<(HttpStatusCode responseCode, string uploadedUrl, bool isLocalFile)> RequestContentUpload(string localFilePath);
     }
 }
--- IRequestContentUploader.cs
+++ IRequestContentUploader.cs
@@ -3,11 +3,12 @@
 namespace PluginBase
 {
     /// <summary>
-    /// Inheriot this class, if you require content upload services
-    /// You can request app to upload content to user chosen CDN. You can call this interface withotu checking if the file is already uploaded,
-    /// the interface will return immediately if so. If file is local file, it is uploaded. Note that uploading might fails, so catch the http status code
+    /// Inherit this class, if you require content upload services
+    /// You can request app to upload content to user chosen CDN. You can call this interface without checking if the file is already uploaded,
+    /// the interface will return immediately if so. File should always be the local shource file, let interface implementation do it's stuff. Note that uploading might fail, so catch the http status code
     /// User might have not activated any content delivery methods, in that case interface will return true is 'is local file' and you must handle error message for the user
     /// Preferred error message is: 'Image needs to be publicly availabe link or you must enable content delivert from Settings'
+    /// It is recommended that in your payload, only show the local source file for user and not store the url given from interface, since it might be temporary link
     /// </summary>
     public interface IRequestContentUploader
     {
