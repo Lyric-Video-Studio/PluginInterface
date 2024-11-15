225dbfab9eba5afcc4159442d02f6c15d88eb975
Matti Pulkkinen <pulkkinen@ultimatium.com>
2024-11-15 19:10:14 +0200

    Bypass storage when project is rendering. - At this stage, plugins are not really called, but Deserialized will be called

--- SecureStorageWrapper.cs
+++ SecureStorageWrapper.cs
@@ -4,16 +4,28 @@ namespace PluginBase
 {
     public static class SecureStorageWrapper
     {
+        public static bool IsRendering;
+
         public static string Get(string key)
         {
+            if (IsRendering)
+            {
+                // Will crash the app if this is called on project rendering, when copies of the project is made
+                return "";
+            }
             Task<string> task = Task.Run(async () => await SecureStorage.Default.GetAsync(key));
             return task.Result;
         }
 
         public static void Set(string key, string value)
         {
+            if (IsRendering)
+            {
+                // Will crash the app if this is called on project rendering, when copies of the project is made
+                return;
+            }
             Task task = Task.Run(async () => await SecureStorage.Default.SetAsync(key, value));
             task.Wait();
         }
-    } 
+    }
 }
