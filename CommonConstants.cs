d1861bd8dc8ae3ac941486252b49d8ebda79f11b
Matti Pulkkinen <pulkkinen@ultimatium.com>
2025-03-11 21:25:56 +0200

    Add webp as supported image format

--- CommonConstants.cs
+++ CommonConstants.cs
@@ -6,7 +6,7 @@
         public const string DefaultIntToStringFormat = "D9";
 
         public static List<string> AllTypes => [.. ImgTypes, .. VideoTypes, .. AudioTypes, .. LyricTypes];
-        public static List<string> ImgTypes => [".png", ".jpg", ".jpeg", ".bmp", ".tga"];
+        public static List<string> ImgTypes => [".png", ".jpg", ".jpeg", ".bmp", ".tga", ".webp"];
         public static List<string> VideoTypes => [".avi", ".mp4", ".divx", ".wmv", ".mts", ".mpg", ".mkv"];
         public static List<string> AudioTypes => [".wav", ".wma", ".mp3", ".ogg", ".rma", ".m4a"];
         public static List<string> LyricTypes => [".txt", ".lrc"];
