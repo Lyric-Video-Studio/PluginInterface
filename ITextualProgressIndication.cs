626576f1a49419ecab630695544cc150e64eb832
Matti Pulkkinen <pulkkinen@ultimatium.com>
2025-04-17 17:09:05 +0300

    Add support for textual progress indication

---
+++ ITextualProgressIndication.cs
@@ -0,0 +1,14 @@
+﻿namespace PluginBase
+{
+    /// <summary>
+    /// Inherit this if your plugin supports reporting progress in form text
+    /// </summary>
+    public interface ITextualProgressIndication
+    {
+        /// <summary>
+        /// Application gives you textual progress action to invoke. Use it if (video) service provides some state info, like "queuing", "processing".
+        /// Currently is shown only for video plugins in edit view
+        /// </summary>
+        void SetTextProgressCallback(Action<string> action);
+    }
+}
