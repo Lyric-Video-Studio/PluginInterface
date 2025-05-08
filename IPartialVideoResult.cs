8898e433847b3d3b362e0c8d5de5f32e9769bf12
Matti Pulkkinen <pulkkinen@ultimatium.com>
2025-05-08 16:35:44 +0300

    Add support for partial videos (framepack)

---
+++ IPartialVideoResult.cs
@@ -0,0 +1,10 @@
+﻿namespace PluginBase
+{
+    /// <summary>
+    /// Inherit this if your video plugin provides partial resutls, like FramePack. Meaning it can output first 1sec etc
+    /// </summary>
+    public interface IPartialVideoFileResult
+    {
+        void SetPartialVideoFileResultCallback(Action<string> partialVideoResult);
+    }
+}
