2cfd3049c1c000023b8f864a9984f18653e7d9e8
Matti Pulkkinen <matti.ultimatium@gmail.com>
2025-05-24 15:28:36 +0300

    Support for overriding secure storage

---
+++ ISecureStorageWrapper.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PluginBase
+{
+    /// <summary>
+    /// This wrapper hides the implementation on LVS secure storage. This is for future use, DO NOT USE THIS YET!!!s
+    /// </summary>
+
+    public interface ISecureStorageWrapper
+    {
+        public bool IsRendering {get;set;}
+        public string Get(string key);
+        public void Set(string key, string value);
+    }
+}
