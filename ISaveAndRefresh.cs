52008f3647a266f83bbdf09ba37cf2734355eebe
Matti Pulkkinen <pulkkinen@ultimatium.com>
2025-07-14 17:53:26 +0300

    Support for storing connection settings from the plugin

--- ISaveAndRefresh.cs
+++ ISaveAndRefresh.cs
@@ -10,4 +10,13 @@
     {
         void SetSaveAndRefreshCallback(Action saveAndRefreshCallback);
     }
+
+    /// <summary>
+    /// Inherit this if your plugin modifies connection settings. Connection settigs class can be used for caching data, for example. 
+    /// For example, your plugins gets some dynamic data for comboboxes, it might not be neede to refresh on every startup
+    /// </summary>
+    public interface ISaveConnectionSettings
+    {
+        void SetSaveConnectionSettingsCallback(Action<object> saveConnectionSettings);
+    }
 }
