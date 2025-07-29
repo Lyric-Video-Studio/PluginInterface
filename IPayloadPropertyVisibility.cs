3a8f58b85fb710b65eaa7db5e11b18054adfa088
Matti Pulkkinen <pulkkinen@ultimatium.com>
2025-07-29 19:07:50 +0300

    Support for validating both payloads at once

---
+++ IPayloadPropertyVisibility.cs
@@ -0,0 +1,11 @@
+﻿namespace PluginBase
+{
+    /// <summary>
+    /// Inherit track or item payload from this interface to enable property based visibility
+    /// Function is called when something in iether of the payloads changes. This works for functions as well
+    /// </summary>
+    public interface IPayloadPropertyVisibility
+    {
+        bool ShouldPropertyBeVisible(string propertyName, object trackPayload, object itemPayload);
+    }
+}
---
+++ IValidateBothPayloads.cs
@@ -0,0 +1,11 @@
+﻿namespace PluginBase
+{
+    /// <summary>
+    /// Inherit if validation depends on both payloads. For example track and item payloads contain list of items
+    /// But only certain amount in total is ok
+    /// </summary>
+    public interface IValidateBothPayloads
+    {
+        public (bool payloadOk, string reasonIfNot) ValidatePayloads(object trackPaylod, object itemPayload);
+    }
+}
