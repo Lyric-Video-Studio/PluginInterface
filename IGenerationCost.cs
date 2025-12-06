40f378e5aa1cc08d81bfc8080aee1fd0e777bae5
Matti Pulkkinen <pulkkinen@ultimatium.com>
2025-12-06 11:05:20 +0200

    Add support for generation cost

---
+++ IGenerationCost.cs
@@ -0,0 +1,13 @@
+﻿namespace PluginBase
+{
+    /// <summary>
+    /// Inherit if plugin supports showing cost of generation. Preferrably before the generate click, if possible :)
+    /// </summary>
+    public interface IGenerationCost
+    {
+        /// <summary>
+        /// Invoke this action when cost changes and ui will update for the cost label
+        /// </summary>
+        public void SetShowCostAction(Action<string> cost);
+    }
+}
