031bd20a2df1643ac1408895e6001286957971d9
Matti Pulkkinen <pulkkinen@ultimatium.com>
2026-01-04 10:02:51 +0200

    Support for payload from model string. Only used internally / by request

---
+++ IPayloadFromModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginBase
+{
+    public interface ITrackPayloadFromModel
+    {
+        /// <summary>
+        /// Create new track payload based on text.
+        /// </summary>
+        object TrackPayloadFromModel(string model);
+    }
+}
