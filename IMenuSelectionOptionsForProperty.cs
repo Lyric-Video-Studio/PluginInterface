e2d221ec6b653a9eb55d41c6a9548946efd34b8b
Matti Pulkkinen <pulkkinen@ultimatium.com>
2026-01-10 11:19:05 +0200

    Support for displaying menu structure in plugin views

---
+++ IMenuSelectionOptionsForProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginBase
+{
+    public interface IMenuSelectionOptionsForProperty
+    {
+        /// <summary>
+        /// Return selection options if property should be shown as menu tree. If you plugin inherits this for property, it will be used instead of IPropertyOptionsProvider FOR THAT SPECIFIC property
+        /// return empty dictionary if property does not wish to be displayed as menu / tree structure
+        /// </summary>
+        public Task<Dictionary<string, string[]>> MenuSelectionOptionsForProperty(string propertyName);
+
+    }
+}
