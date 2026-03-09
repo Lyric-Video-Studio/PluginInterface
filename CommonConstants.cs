29f18027a71935c0f663be22fb2441f9b9ec37e9
Matti Pulkkinen <pulkkinen@ultimatium.com>
2026-03-09 19:27:50 +0200

    Add support for .mov

--- CommonConstants.cs
+++ CommonConstants.cs
@@ -7,7 +7,7 @@
 
         public static List<string> AllTypes => [.. ImgTypes, .. VideoTypes, .. AudioTypes, .. LyricTypes];
         public static List<string> ImgTypes => [".png", ".jpg", ".jpeg", ".bmp", ".tga", ".webp"];
-        public static List<string> VideoTypes => [".avi", ".mp4", ".divx", ".wmv", ".mts", ".mpg", ".mkv"];
+        public static List<string> VideoTypes => [".avi", ".mp4", ".divx", ".wmv", ".mts", ".mpg", ".mkv", ".mov"];
         public static List<string> AudioTypes => [".wav", ".wma", ".mp3", ".ogg", ".rma", ".m4a"];
         public static List<string> LyricTypes => [".txt", ".lrc", ".vtt", ".srt", ".tsv"];
 
