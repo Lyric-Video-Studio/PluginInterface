f30ebd4f4438d3815393e603118c7baa5470fb07
Matti Pulkkinen <pulkkinen@ultimatium.com>
2026-06-16 09:21:55 +0300

    Preparation for MCP support

---
+++ IAllowMcpGeneration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginBase
+{
+    /// <summary>
+    /// If you want your plugin to be connected to MCP server, inherit ConnectionSettings (or similar)-classs from this
+    /// Only plugins that have this intereface in the settings are qualified for MCP usage. User has option to toggle plugin acces on/off
+    /// In general, if plugin generations cost money (API), default access to false. Local plugins are usually good for defualting true
+    /// </summary>
+    public interface IAllowMcpGeneration
+    {
+        bool AlloMcpAccess { get; set; }
+    }
+}
